7f2a72b29d7ea4f5465cbe3c74d761f34789b49c
Henrique20o <137103470+Henrique20o@users.noreply.github.com>
2023-09-13 22:00:37 -0300

    toma essa porra ai

--- Site/user_site/interesses.php
+++ Site/user_site/interesses.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,21 +9,19 @@
     <link href="../node_modules/bootstrap/compiler/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" href="../assets/style/style.css">
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <title>Hello, world!</title>
+    <title>SEN.AI</title>
 </head>
+
 <body>
     <header class="navbar navbar-light">
         <div class="container">
             <a href="#" class="navbar-brand h1 fixed">SEN.AI</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                 <div class="offcanvas-header">
-                    <h5 class="offcanvas-title user" id="offcanvasNavbarLabel"><a href=""
-                            class="nav-link active">McLovin</a>
+                    <h5 class="offcanvas-title user" id="offcanvasNavbarLabel"><a href="" class="nav-link active">McLovin</a>
                         <img class="userimg" src="../assets/img/User.jpg"></img>
                     </h5><br>
                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -50,10 +49,11 @@
         </div>
     </header>
 
-    <div class="container-fluid form1">
+    <div class="form1">
         <h1>Selecione um interesse</h1>
         <form class="form-floating" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-            <select class="form-select" name="interesses" id="interesses">
+            <select class="form-select" name="interesses" id="interesses" required>
+                <option value="">------</option>
                 <option value="Esportes">Esportes</option>
                 <option value="Musica">Música</option>
                 <option value="Viagem">Viagem</option>
@@ -72,7 +72,7 @@
             </select>
             <input id="submit" class="ui primary button enviar" type="submit" name="enviar" value="ENVIAR">
         </form>
-        
+
         <?php
         // Verifique se o formulário foi submetido
         if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["enviar"])) {
@@ -91,7 +91,7 @@
                     FROM postagens p
                     INNER JOIN interesses i ON p.interesse_id = i.ID
                     WHERE i.interesse = '$interesseSelecionado'";
-            
+
             $result = $conexao->query($sql);
 
             // Verifique se a consulta foi bem-sucedida
@@ -105,11 +105,8 @@
                 $menosDe100 = $row["menos_de_100"];
                 $maisDe500 = $row["mais_de_500"];
                 $maisDe1500 = $row["mais_de_1500"];
-                $maisDe2000 = $row["mais_de_2000"];
-
-;
+                $maisDe2000 = $row["mais_de_2000"];;
             }
-            $interesseSelecionado = $_POST["interesses"];
 
             // Consulta SQL para contar posts com base nos compartilhamentos e interesse selecionado
             $sql = "SELECT 
@@ -120,7 +117,7 @@
                     FROM postagens p
                     INNER JOIN interesses i ON p.interesse_id = i.ID
                     WHERE i.interesse = '$interesseSelecionado'";
-            
+
             $result = $conexao->query($sql);
 
             // Verifique se a consulta foi bem-sucedida
@@ -135,87 +132,110 @@
                 $mais500 = $row["mais_de_500"];
                 $mais1000 = $row["mais_de_1000"];
                 $mais3000 = $row["mais_de_3000"];
-
             } else {
                 echo "Nenhuma postagem encontrada para o interesse selecionado.";
             }
 
+            // Consulta SQL para obter a quantidade de usuários em cada faixa etária
+            $sql = "SELECT 
+             idade
+        FROM postagens p
+        INNER JOIN usuarios u ON p.user_id = u.id
+        INNER JOIN interesses i ON p.interesse_id = i.id
+        WHERE i.interesse = '$interesseSelecionado'";
+
+            $result = $conexao->query($sql);
+
+            if ($result === false) {
+                die("Erro na consulta SQL: " . $conexao->error);
+            }
+
+            // Inicialize variáveis para contar o número de usuários em cada faixa etária
+            $menor18 = 0;
+            $entre18e24 = 0;
+            $entre25e35 = 0;
+            $maior35 = 0;
+
+            while ($row = $result->fetch_assoc()) {
+                $idade = $row["idade"];
+
+                if ($idade < 18) {
+                    $menor18++;
+                } elseif ($idade >= 18 && $idade <= 24) {
+                    $entre18e24++;
+                } elseif ($idade >= 25 && $idade <= 35) {
+                    $entre25e35++;
+                } else {
+                    $maior35++;
+                }
+            }
             // Feche a conexão com o banco de dados
             $conexao->close();
 
         }
         ?>
-    <div class="container-fluid contn">
-        <div id="chart_div" style="width: 450px; height: 250px;"></div>
-        <div id="chart_div2" style="width: 500px; height: 250px;"></div>
-        <div id="chart_div3" style="width: 450px; height: 250px;"></div>
-        <div id="chart_div4" style="width: 450px; height: 250px;"></div>
-    </div>
+        <div class="container contn">
+            <div id="chart_div" style="width: 450px; height: 250px;"></div>
+            <div id="chart_div2" style="width: 500px; height: 250px;"></div>
+            <div id="chart_div3" style="width: 450px; height: 250px;"></div>
+            <div id="chart_div4" style="width: 450px; height: 250px;"></div>
+        </div>
 
-<script type="text/javascript">
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
+        <script type="text/javascript">
+            google.charts.load('current', {
+                'packages': ['corechart']
+            });
+            google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Curtidas');
-        data.addColumn('number', 'Quantidade');
-        data.addRows([
-            ['Menos de 100', <?php echo $menosDe100; ?>],
-            ['Mais de 500', <?php echo $maisDe500; ?>],
-            ['Mais de 1500', <?php echo $maisDe1500; ?>],
-            ['Mais de 2000', <?php echo $maisDe2000; ?>]
+            function drawChart() {
+                var data = new google.visualization.DataTable();
+                data.addColumn('string', 'Curtidas');
+                data.addColumn('number', 'Quantidade');
+                data.addRows([
+                    ['Menos de 100', <?php echo $menosDe100; ?>],
+                    ['Mais de 500', <?php echo $maisDe500; ?>],
+                    ['Mais de 1500', <?php echo $maisDe1500; ?>],
+                    ['Mais de 2000', <?php echo $maisDe2000; ?>]
+                ]);
+
+                var options = {
+                    title: 'Estatísticas de Curtidas',
+                    width: 500,
+                    height: 350
+                };
+
+                var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+
+                chart.draw(data, options);
+            }
+
+            google.charts.load('current', {
+        packages: ['corechart']
+    });
+    google.charts.setOnLoadCallback(drawAgeDistributionChart);
+
+    function drawAgeDistributionChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Faixa Etária', 'Número de Usuários'],
+            ['Menor de 18', <?php echo $menor18; ?>],
+            ['18-24', <?php echo $entre18e24; ?>],
+            ['25-35', <?php echo $entre25e35; ?>],
+            ['Maior de 35', <?php echo $maior35; ?>]
         ]);
 
         var options = {
-            title: 'Estatísticas de Curtidas',
+            title: 'Distribuição Etária dos Usuários',
             width: 500,
             height: 350
         };
 
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div3'));
 
         chart.draw(data, options);
     }
-
-    google.charts.load('current', {
-  packages: ['corechart', 'bar']
-});
-
-google.charts.load("current", {packages:['corechart']});
-google.charts.setOnLoadCallback(chartDraw);
-
-    function chartDraw() {
-      var data = google.visualization.arrayToDataTable([
-        ["Seleção", "Quantidade", { role: "style" } ],
-        ["Menos que 100", <?php echo $menos100; ?>, "blue"],
-        ["Mais que 500", <?php echo $mais500; ?>, "blue"],
-        ["Mais que 1000", <?php echo $mais1000; ?>, "blue"],
-        ["Mais 3000", <?php echo $mais3000; ?>, "blue"]
-      ]);
-
-      var view = new google.visualization.DataView(data);
-      view.setColumns([0, 1,
-                       { calc: "stringify",
-                         sourceColumn: 1,
-                         type: "string",
-                         role: "annotation" },
-                       2]);
-
-      var options = {
-        title: "Quantidade de postagens com compartilhamentos",
-        width: 600,
-        height: 400,
-        bar: {groupWidth: "95%"},
-        legend: { position: "none" },
-      };
-      var chart = new google.visualization.ColumnChart(document.getElementById("chart_div2"));
-      chart.draw(view, options);
-    }
-
-</script>
-<script src="../node_modules/bootstrap/dist/js/bootstrap.js"></script>
-
+        </script>
+        <script src="../node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    </div>
 </body>
-</html>
 
+</html>
